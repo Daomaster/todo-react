8ea299d46a87026711d6047f782dbd2535561c16
Daoyun Zeng <daoyun.zeng@gmail.com>
2020-05-04 18:00:59 +0800

    feat: Initial commit with a scaffold server side code.

---
+++ server.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import {ApolloServer} from "apollo-server-express";
+import apolloConfig from "./src/schemas/indext";
+
+const app = express();
+
+// add all the middleware
+app.use('*', cors());
+app.use(compression());
+
+// init the apollo server
+const server = new ApolloServer(apolloConfig);
+server.applyMiddleware({app});
+
+// construct the mongo connection string
+const mongoUrl = `mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+}@cluster0-replz.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+// init the mongo connection
+mongoose.connect(mongoUrl, { useNewUrlParser: true , useUnifiedTopology: true })
+    .then(() => {
+        // start the server once mongo is connected
+        app.listen(8000);
+    })
+    .catch(err => {
+        console.log(err)
+    });
